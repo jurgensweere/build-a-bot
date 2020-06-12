54a614bbe08b86c62bb251778a426f4dfbfe3de9
Jurgen Sweere <jurgen.sweere@nl.abnamro.com>
2020-06-12 16:38:39 +0200

    Filters and Directives

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -3,7 +3,7 @@
     <img @click="showPartInfo" :src="selectedPart.src" title="arm" />
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span v-pin:position.top.left class="sale" v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
@@ -79,9 +79,6 @@ export default {
   cursor: pointer;
 }
 .sale {
-  position: absolute;
-  bottom: 5px;
-  right: 5px;
   color: white;
   background-color: red;
   padding: 3px;
---
+++ src/shared/pin-directive.js
@@ -0,0 +1,8 @@
+export default function (element, binding) {
+  if (binding.arg !== 'position') return;
+
+  Object.keys(binding.modifiers).forEach((key) => {
+    element.style[key] = '5px';
+  });
+  element.style.position = 'absolute';
+}
